3fbd9df73833941ecf7f5cd9c3044e535f4e17a6
angelokeirsebilck <angelo.keirsebilck1@telenet.be>
2020-12-08 18:31:19 +0100

    Fix db env

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGDODB_URL, { useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useCreateIndex: true });
